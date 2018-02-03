6c4070ef8ba08af494e36887ee580699f6ab4cee
nomadlife <joonwoo3816@gmail.com>
2018-02-04 01:49:18 +0900

    clock_hour_needle_issue_fixed

--- jsproject02_clock/clock0.js
+++ jsproject02_clock/clock0.js
@@ -7,15 +7,15 @@ function setDate(){
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360 + 90);
+  const secondsDegrees = ((seconds*6) + 90);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
-  const minsDegrees = ((mins/60)*360+90);
+  const minsDegrees = ((mins*6)+90);
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = ((mins/12)*360+90);
+  const hoursDegrees = ((hours*30)+(mins/12)+90);
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
   console.log(hours,mins,seconds);
