f404bb9961902b7ed284048b4b89e7e8569eb2b3
nomadlife <joonwoo3816@gmail.com>
2018-02-05 23:50:12 +0900

    drumkit_updated

---
+++ jsproject01_drumkit/script.js
@@ -0,0 +1,24 @@
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log("audio:",audio,"key:",key);
+  if(!audio) return;// stop the  from running all together.
+  audio.currentTime = 0;
+  audio.play();
+  // key.addClass('playing')
+  key.classList.add('playing');
+  // key.classList.remove('playing');
+  // key.classList.toggle('playing');
+
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  console.log("this:",this);
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown',playSound);
