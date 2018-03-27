7dff8451bbddf5118b70e996f3dae1d4d0313e83
nomadlife <joonwoo3816@gmail.com>
2018-03-27 10:02:35 +0900

    js project 10 updated, ongoing

--- jsproject10_hold_shift/project10.js
+++ jsproject10_hold_shift/project10.js
@@ -1,6 +1,24 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
+
 function handleCheck(e){
-  console.log(e);
+  let inBetween = false;
+  if (e.shiftKey && this.checked){
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if(checkbox === this || checkbox === lastChecked){
+        inBetween = !inBetween;
+        console.log('inBetween border-------');
+      }
+      if(inBetween){
+        checkbox.checked = true;
+      }
+    })
+  }
+  if(this.check){
+    lastChecked = this;
+    console.log("lastChecked:",lastChecked);
+  }
 }
 checkboxes.forEach(checkbox => checkbox.addEventListener('click',handleCheck));
