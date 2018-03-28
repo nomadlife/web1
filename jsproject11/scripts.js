e229f51a01aaab89f15e7fd446b3c8d039bbfdb2
nomadlife <joonwoo3816@gmail.com>
2018-03-28 18:42:47 +0900

    js project 11 updated

--- jsproject11/scripts.js
+++ jsproject11/scripts.js
@@ -1,9 +1,17 @@
 // get element
 const player = document.querySelector('.player');
-const video = player.querySelector(.'viewer');
-
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
 
 // build out functions
+function togglePlay(){
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
+function updateButton(){
+  console.log('update the button')
+}
 
 // hook up the event listners
