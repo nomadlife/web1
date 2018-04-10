a0c8ebb3323350ead7109d792181bff9be5df70d
nomadlife <joonwoo3816@gmail.com>
2018-04-10 10:43:09 +0900

    js project 19 added

---
+++ jsproject19/scripts.js
@@ -0,0 +1,32 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+	navigator.mediaDevices.getUserMedia({video:true, audio:false})
+	.then(localMediaStream => {
+		console.log(localMediaStream);
+		video.src = window.URL.createObjectURL(localMediaStream);
+		video.play();
+	})
+	.catch(err => {
+		console.error(`OH NO!!`,err);
+	})
+}
+
+function paintToCanvas(){
+	const width = video.videoWidth;
+	const height = video.videoGeight;
+	canvas.width = width;
+	canvas.height = height;
+	console.log(width, height);
+
+	setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+	}, 100);
+
+}
+
+getVideo();
