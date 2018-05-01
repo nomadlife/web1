4cce60b4c62b6ad4c127226829f2c8d30e102a5e
nomadlife <joonwoo3816@gmail.com>
2018-05-01 16:57:52 +0900

    html5 local video/audio file with subtitle test -1

---
+++ jsproject11/script3.js
@@ -0,0 +1,40 @@
+(function localFileVideoPlayer() {
+	'use strict'
+  var URL = window.URL || window.webkitURL
+  var displayMessage = function (message, isError) {
+    var element = document.querySelector('#message')
+    element.innerHTML = message
+    element.className = isError ? 'error' : 'info'
+  }
+  
+  var playSelectedFile = function (event) {
+    var file = this.files[0]
+    var type = file.type
+    var videoNode = document.querySelector('video')
+    var canPlay = videoNode.canPlayType(type)
+    if (canPlay === '') canPlay = 'no'
+    var message = 'Can play type "' + type + '": ' + canPlay
+    var isError = canPlay === 'no'
+    displayMessage(message, isError)
+
+    if (isError) {
+      return
+    }
+
+    var fileURL = URL.createObjectURL(file)
+    videoNode.src = fileURL
+  }
+  
+  audio_file.onchange = function(){
+  var files = this.files;
+  var file = URL.createObjectURL(files[0]); 
+              audio_player.src = file; 
+  audio_player.play();
+};
+
+
+
+  
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, false)
+})()
